78365e3381665ccec1be2f64e7b23644a9f4078a
SamDGit <samuel.dietrich@haeger-consulting.de>
2022-11-07 17:00:10 +0100

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World! (c) by samd'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
