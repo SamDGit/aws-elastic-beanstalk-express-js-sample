fa3882d912c4cd4da62a202bebf0be42c72c6d1e
SamDGit <samuel.dietrich@haeger-consulting.de>
2022-11-08 12:24:42 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! (c) by samd'));
+app.get('/', (req, res) => res.send('Hello World! (c) 2022 by samd'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
